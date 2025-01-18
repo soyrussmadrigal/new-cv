e2bc863b565658a56816c69e4bee0c83b4ebced1
Russ Madrigal <yourgitemail@example.com>
2025-01-18 11:42:07 -0600

    301 redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  async redirects() {
+    return [
+      {
+        source: '/acerca',
+        destination: '/', // Reemplaza con la URL de destino
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
