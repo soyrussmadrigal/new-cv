7f8d1e43195c1945ceb29967d4f8ef06c09beee3
Russ Madrigal <yourgitemail@example.com>
2025-01-29 06:54:21 -0600

    Remove redundant 301 redirect for /test from next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,9 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
   async redirects() {
     return [
       {
         source: '/acerca',
-        destination: '/', // Reemplaza con la URL de destino
-        permanent: true,
-      },
-      {
-        source: '/test',
         destination: '/', // Reemplaza con la URL de destino
         permanent: true,
       },
