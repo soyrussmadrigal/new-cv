2313a4f32deb3ad371e41db63291fbc807978cab
Russ Madrigal <yourgitemail@example.com>
2025-02-11 17:34:34 -0600

    Add permanent redirects for '/acerca' and '/tiene-el-keyword-research-impacto-en-el-seo-john-mueller-dice-no' to root URL

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,13 @@ const nextConfig = {
   async redirects() {
     return [
       {
-        source: '/acerca',
-        destination: '/', // Reemplaza con la URL de destino
+        source: "/acerca",
+        destination: "/", // Reemplaza con la URL de destino
+        permanent: true,
+      },
+      {
+        source: "/tiene-el-keyword-research-impacto-en-el-seo-john-mueller-dice-no",
+        destination: "/", // Reemplaza con la URL de destino
         permanent: true,
       },
     ];
